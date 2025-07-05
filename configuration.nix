9c98033c4a5fbb46fc730730bac2e2fb37e1bd2e
Psyhackological <konradkon@duck.com>
2025-07-05 12:58:57 +0200

    Working NVIDIA GPU, hardware acc but not yet figured out Blender CUDA

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,37 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
- 
-{ config, pkgs, ... }:
- 
+# Edit this configuration file to define what should be installed on your system.  Help is available in the configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
 {
   imports =
     [ # Include the results of the hardware scan.
+    <nixos-hardware/system76>
       ./hardware-configuration.nix
     ];
- 
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
- 
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
- 
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
- 
+
   # Enable networking
   networking.networkmanager.enable = true;
- 
+
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
- 
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
- 
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pl_PL.UTF-8";
     LC_IDENTIFICATION = "pl_PL.UTF-8";
@@ -41,101 +43,132 @@
     LC_TELEPHONE = "pl_PL.UTF-8";
     LC_TIME = "pl_PL.UTF-8";
   };
- 
- 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-  nix.extraOptions = ''
-  min-free = ${toString (512 * 1024 * 1024)}
-  max-free = ${toString (2048 * 1024 * 1024)}
-  '';
- 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
- 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
- 
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "pl";
     variant = "";
   };
- 
+
   # Configure console keymap
   console.keyMap = "pl2";
- 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
- 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
- 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
- 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
- 
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.konradkon = {
     isNormalUser = true;
     description = "Konrad Konieczny";
     extraGroups = [ "networkmanager" "wheel" ];
+
+    # WIP PACKAGES
     packages = with pkgs; [
-      kdePackages.kate
-      pkgs.mullvad-browser
-      mpv
-      kitty
+      # Text Editors
+      vim
+      neovim
+      helix
+      calibre
+      libreoffice-fresh
+      signal-desktop
+
+      # Core
+      fuzzel
+
+      # Terminals
       alacritty
+      kitty
+
+      # Mullvad
+      mullvad-browser
+      mullvad
+
+      # Media
       mpv
-      gimp
-      cudaPackages.cudatoolkit
+      tidal-hifi
+
+      # Creative
       blender
       inkscape
-      protonup-qt
+      gimp3
+
+      # Other
       fastfetch
-      wl-clipboard
+
+      # Gaming
+      mangohud
+      protonup
     ];
   };
- 
-  # Install firefox.
-  #programs.firefox.enable = false;
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
- 
+
+  
+
+  # === WIP nix experimental ===
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  # === WIP PipeWire
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true; # if not already enabled
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+  # === WIP Gnome ===
+  services.xserver.displayManager.gdm.enable = true;
+
+  services.printing.enable = true;
+
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
- 
+  nixpkgs.config = {
+    allowUnfree = true;
+    # cudaSupport = true; # It causes OOM killer
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  # === WIP SYSTEM PACKAGES ===
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    helix
+    wl-clipboard
+    xwayland-satellite
+    wget
+    vim
     lshw
-    mangohud
+    home-manager
+    mako
+    waybar
+    swaybg
+    swayidle
+    swaylock
+    brightnessctl
     nvtopPackages.nvidia
+    cudaPackages.cudatoolkit
   ];
- 
+
+  # === WIP PROGRAMS ENABLE
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+  };
+  programs.niri.enable = true;
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
+  programs.ssh.startAgent = true;
+
+  programs.fish.enable = true;
+  users.users.konradkon.shell = pkgs.fish;
+
+  # === WIP ENV ===
+  environment.variables.GTK_THEME = "Adwaita:dark";
+  environment.variables.EDITOR= "hx";
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -143,72 +176,94 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
- 
+
   # List services that you want to enable:
- 
+
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
- 
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
- 
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
- 
-  # === NVIDIA ===
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [nvidia-vaapi-driver];
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+  # == WIP FONTS ===
+  fonts.enableDefaultPackages = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  # == NIX GARBAGE ==
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 35d";
   };
- 
+
+  nix.extraOptions = ''
+    min-free = ${toString (2048 * 1024 * 1024)}
+    max-free = ${toString (10240 * 1024 * 1024)}
+  '';
+
+  nix.settings.auto-optimise-store = true;
+
+  # == NIX UPGRADE ==
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+    channel = https://nixos.org/channels/nixos-25.05;
+  };
+  # === WIP GRAPHICS ==
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+
+  # Enable OpenGL
+  hardware.graphics = { # hardware.graphics since NixOS 24.11
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
- 
+  services.xserver.videoDrivers = [
+    "modesetting"  # example for Intel iGPU; use "amdgpu" here instead if your iGPU is AMD
+    "nvidia"
+  ];
+
   hardware.nvidia = {
+    # Modsettings is required
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
     prime = {
-      sync.enable = true;
-      # Make sure to use the correct Bus ID values for your system!
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
- 
-    # Modesetting is required.
-    modesetting.enable = true;
- 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
- 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
- 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = true;
- 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
- 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
- 
-}
 
+  };
+}
